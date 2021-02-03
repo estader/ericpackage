30af3aa7f572bf8590da6d69967dee601c521fc3
Eric Esteves <eeaderne@gmail.com>
2021-02-03 13:10:21 -0300

    novo modulo results_interpretation.py

---
+++ ericpackage/results_interpretation.py
@@ -0,0 +1,24 @@
+
+import lime
+import lime.lime_tabular
+
+
+def ri_lime_tabular(X_train, X_test, modelo, index_sample, class_names, modo = 'regression'):
+    """
+        Interpretador para dados tabulares
+        Modo regressão ou classificação: regression | classification
+    """
+    explainer = lime.lime_tabular.LimeTabularExplainer(X_train.values, feature_names = X_train.columns.values,
+                                                       class_names= class_names,
+                                                       verbose=True,
+                                                       mode=modo)
+
+    if modo =='regression':
+        exp = explainer.explain_instance(X_test.iloc[index_sample], modelo.predict,
+                                         num_features = len(X_train.columns))
+    else:
+        exp = explainer.explain_instance(X_test.iloc[index_sample], modelo.predict_proba,
+                                         num_features = len(X_train.columns))
+    exp.show_in_notebook(show_table=True)
+    exp.as_list()
+    return exp
