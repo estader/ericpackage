7ad48870dd970f99e917f287ebd8e48eac46834e
Eric Aderne <eeaderne@gmail.com>
2022-03-15 20:13:06 -0300

    updates

--- setup.py
+++ setup.py
@@ -1,35 +1,48 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 from pathlib import Path
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
-# Packages required for this module to be executed
-#def list_reqs(fname='requirements.txt'):
-#    with open(fname) as fd:
-#        return fd.read().splitlines()
-    
-# Read the requirements
 source_root = Path(".")
 with (source_root / "requirements.txt").open(encoding="utf8") as f:
     requirements = f.readlines()
 
-
 setup(
-    name="ericpackage", 
+    name="erpack", 
     version="0.0.1",
     author="Eric Aderne",
     author_email="eeaderne@gmail.com",
-    description="Funções da construção de um pipeline em projetos com Aprendizado e Máquina",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    description='Data Science PaSckage',
     url="git@github.com:estader/ericpackage",
     install_requires=requirements,
-    packages=['ericpackage'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    license='unlicense',
+    package_dir= {
+        'erpack': '',
+        'erpack.automl': 'erpack/automl',
+        'erpack.data_cleaning': 'erpack/data_cleaning',
+        'erpack.error_analysis': 'erpack/error_analysis',
+        'erpack.exploratory_analysis': 'erpack/exploratory_analysis',
+        'erpack.feature_engineering': 'erpack/feature_engineering',
+        'erpack.feature_selection': 'erpack/feature_selection',
+        'erpack.monitoring': 'erpack/monitoring',
+        'erpack.production_pipeline': 'erpack/production_pipeline',
+        'erpack.quality_check': 'erpack/quality_check',
+        'erpack.result_interpretation': 'erpack/result_interpretation',
+        'erpack.train_test_split': 'erpack/train_test_split',},
+    packages = ['erpack',
+                'erpack.automl',
+                'erpack.data_cleaning',
+                'erpack.error_analysis',
+                'erpack.exploratory_analysis',
+                'erpack.feature_engineering',
+                'erpack.feature_selection',
+                'erpack.monitoring',
+                'erpack.production_pipeline',
+                'erpack.quality_check',
+                'erpack.result_interpretation',
+                'erpack.train_test_split'],
+    zip_safe=False,
+    python_requires='>=3.6'
 )
+
+
+
