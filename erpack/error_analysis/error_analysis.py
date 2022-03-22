1cd8a167cca262ceffac813297dfdc2dfd821d04
Eric Aderne <eeaderne@gmail.com>
2022-03-21 23:32:18 -0300

    update automl classifier

--- erpack/error_analysis/error_analysis.py
+++ erpack/error_analysis/error_analysis.py
@@ -2,6 +2,7 @@
 from mlxtend.evaluate import bias_variance_decomp
 import pandas as pd
 import seaborn as sns
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
 def decomposicao_bias_variancia(modelo, X_train, y_train, X_test, y_test):
@@ -15,25 +16,53 @@ def decomposicao_bias_variancia(modelo, X_train, y_train, X_test, y_test):
     return
 
 
-def report_features_erros_modelos(modelos, X_val, y_val, df_predicoes, target):
-    top_features = modelos.top_features.iloc[0]#.strip('][').replace("'",'').split(', ') 
-    df_predicoes = pd.concat([df_predicoes, y_val.reset_index(drop=True)], axis=1, join='inner')
-    df_predicoes = pd.concat([X_val[top_features].reset_index(drop=True), df_predicoes], axis=1)
-    
-    for i in list(modelos.tipo):
-        df_predicoes[i+'_real_dif'] = df_predicoes.apply(lambda x: abs(x[i] - x[target]), axis=1)
+def report_features_erros_modelos(modelos, X_val, y_val, df_predicoes, target, modo='regression'):
+
+    if modo == 'regression':
+        top_features = modelos.top_features.iloc[0]#.strip('][').replace("'",'').split(', ') 
+        df_predicoes = pd.concat([df_predicoes, y_val.reset_index(drop=True)], axis=1, join='inner')
+        df_predicoes = pd.concat([X_val[top_features].reset_index(drop=True), df_predicoes], axis=1)
         
-    plt.figure(figsize=(10,10))
-    plt.rcParams['font.size'] = 10
-    ax1 = sns.scatterplot(data = df_predicoes,sizes=20, x=target, y=modelos.tipo.iloc[0], alpha=0.5, s=15)
-    ax1.plot([0,int(df_predicoes[target].max())],[0,int(df_predicoes[target].max())], '--')
-    ax1.set_title('Valores reais e previsões')
-    
-    plt.figure(figsize=(10,10))
-    ax1 = sns.lineplot(data = df_predicoes, x = target, y=modelos.tipo.iloc[0]+'_real_dif')
-    ax1.set_title('Erros x faixa do target')
+        for i in list(modelos.tipo):
+            df_predicoes[i+'_real_dif'] = df_predicoes.apply(lambda x: abs(x[i] - x[target]), axis=1)
+            
+        plt.figure(figsize=(10,10))
+        plt.rcParams['font.size'] = 10
+        ax1 = sns.scatterplot(data = df_predicoes,sizes=20, x=target, y=modelos.tipo.iloc[0], alpha=0.5, s=15)
+        ax1.plot([0,int(df_predicoes[target].max())],[0,int(df_predicoes[target].max())], '--')
+        ax1.set_title('Valores reais e previsões')
+        
+        plt.figure(figsize=(10,10))
+        ax1 = sns.lineplot(data = df_predicoes, x = target, y=modelos.tipo.iloc[0]+'_real_dif')
+        ax1.set_title('Erros x faixa do target')
 
-    melhores_pred =df_predicoes.sort_values(modelos.iloc[0,0]+'_real_dif',ascending=True).head(10)
-    piores_pred = df_predicoes.sort_values(modelos.iloc[0,0]+'_real_dif',ascending=False).head(10)
+        melhores_pred =df_predicoes.sort_values(modelos.iloc[0,0]+'_real_dif',ascending=True).head(10)
+        piores_pred = df_predicoes.sort_values(modelos.iloc[0,0]+'_real_dif',ascending=False).head(10)
 
-    return melhores_pred, piores_pred
+        return melhores_pred, piores_pred
+
+    if modo == 'classifier':
+       
+        top_features = modelos.top_features.iloc[0]
+        df_predicoes = pd.concat([df_predicoes, y_val.reset_index(drop=True)], axis=1, join='inner')
+        df_predicoes = pd.concat([X_val[top_features].reset_index(drop=True), df_predicoes], axis=1)
+
+        for i in list(modelos.tipo):
+            df_predicoes[i+'_real_dif'] = df_predicoes.apply(lambda x: abs(x[i] - x[target]), axis=1)
+
+
+        plt.figure(figsize=(10,10))
+        cf_matrix = confusion_matrix(y_val, modelos.tipo.iloc[0])
+        labels = ['True Neg','False Pos','False Neg','True Pos']
+        labels = np.asarray(labels).reshape(2,2)
+        ax1 = sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Blues')
+        ax1.set_title('Erros x faixa do target')
+
+        # acertos
+        melhores_pred = df_predicoes.sort_values(modelos.iloc[0,0]+'_real_dif', ascending=True).head(10)
+
+        # erros
+        piores_pred = df_predicoes.sort_values(modelos.iloc[0,0]+'_real_dif', ascending=False).head(10)
+
+        return melhores_pred, piores_pred
+
